f47d194b2ca9fba0da0c3705ef56e253933b1e0c
matsu30 <18610317m@gmail.com>
2020-10-21 22:31:20 +0900

    床の消去

--- main.js
+++ main.js
@@ -117,75 +117,6 @@ console.log(starring.body.position)
 
         document.addEventListener( 'keydown', onKeyDown, false );
         document.addEventListener( 'keyup', onKeyUp, false );
-    
-   
-        //--------------床----------------------------------------
-　      var floorGeometry = new THREE.PlaneBufferGeometry( 2000, 2000, 100, 100 );
-        floorGeometry.rotateX( - Math.PI / 2 );
-
-
-        // 頂点を変える
-        var position = floorGeometry.attributes.position;
-
-        for ( var i = 0, l = position.count; i < l; i ++ ) {
-
-            vertex.fromBufferAttribute( position, i );
-
-            vertex.x += Math.random() * 20 - 10;
-            vertex.y += Math.random() * 2;
-            vertex.z += Math.random() * 20 - 10;
-
-            position.setXYZ( i, vertex.x, vertex.y, vertex.z );
-
-        }
-
-        // 各面に一点の頂点がある。
-        floorGeometry = floorGeometry.toNonIndexed(); 
-        position = floorGeometry.attributes.position;
-
-        var colors = [];
-        for ( var i = 0, l = position.count; i < l; i ++ ) {
-
-            color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-            colors.push( color.r, color.g, color.b );
-
-        }
-        floorGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
-        var floorMaterial = new THREE.MeshBasicMaterial( { vertexColors: true } );
-        var floor = new THREE.Mesh( floorGeometry, floorMaterial );
-        scene.add( floor );
-
-
-        //--------object--------------------------------------
-        // var boxGeometry = new THREE.BoxBufferGeometry( 20, 20, 20 );
-
-        // // 各面に一点の頂点があるのは床と同じ。色も同じように定義する。
-        // boxGeometry = boxGeometry.toNonIndexed();
-        // position = boxGeometry.attributes.position;
-
-        // colors = [];
-        // for ( var i = 0, l = position.count; i < l; i ++ ) {
-
-        //     color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-        //     colors.push( color.r, color.g, color.b );
-
-        // }
-        // boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
-
-        // for ( var i = 0; i < 50; i ++ ) {
-
-        //     var boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
-        //     boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-
-        //     var box = new THREE.Mesh( boxGeometry, boxMaterial );
-        //     box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
-        //     box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
-        //     box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
-
-        //     scene.add( box );
-        //     objects.push( box );
-
-        // }
 
         const obstacleKeydata = {
             width: 20,
@@ -797,7 +728,6 @@ console.log(starring.body.position)
             repeatY: 0.145,
         })
         scene.add(yuka212.mesh);
-
 
         const yuka213 = new Illusttexture({
             texture:'img/start02.png',
